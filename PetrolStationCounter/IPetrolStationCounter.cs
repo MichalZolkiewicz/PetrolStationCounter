9d5ea9a64fff7b26cf7994b8f1f42742ce36e2f0
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-11-02 23:10:52 +0100

    interface with properties, event and Add methods created

---
+++ PetrolStationCounter/IPetrolStationCounter.cs
@@ -0,0 +1,17 @@
+﻿using static PetrolStationCounter.BasePetrolStationCounter;
+
+namespace PetrolStationCounter
+{
+    public interface IPetrolStationCounter
+    {
+        string Name { get; }
+        string Location { get; }
+
+        public double AddLiters(double liters);
+        public double AddLiters(char liters);
+        public double AddLiters(string liters);
+        Statistics GetStatistics();
+
+        event LitersAddedDelegate LitersAdded;
+    }
+}
