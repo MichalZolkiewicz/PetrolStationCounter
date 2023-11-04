126a0cce585fce4fd25c472a9d7ce81ed69cd651
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-11-04 16:39:14 +0100

    Adding liters to files working program

--- PetrolStationCounter/Program.cs
+++ PetrolStationCounter/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PetrolStationCounter;
+using static PetrolStationCounter.PetrolStationCounterInFile;
+
+Console.WriteLine("Witamy w programie XYZ do wprowadzania ilości sprzedanego paliwa");
+Console.WriteLine("===========================================");
+Console.WriteLine();
+
+var petrolStation = new PetrolStationCounterInFile("BP", "Karków");
+petrolStation.LitersAdded += PetrolStationLitersAdded;
+void PetrolStationLitersAdded(object sender, EventArgs args)
+{
+    Console.WriteLine("Dodano ilość sprzedanego paliwa!");
+}
+
+while(true)
+{
+    Console.WriteLine("Wybierz rodzaj paliwa\nA - 95, B - 98, C - ON, D - ON Ultimate\nAby wyjść wybierz Q");
+    var input = Console.ReadLine();
+    string fileName = petrolStation.ChooseFile(input);
+    
+    Console.WriteLine("Wprowadz ilość sprzedanego paliwa");
+    var litersAdded = Console.ReadLine();
+
+    try
+    {
+        petrolStation.AddLiters(litersAdded, fileName);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"Exception catched: {e.Message}");
+    }
+}
