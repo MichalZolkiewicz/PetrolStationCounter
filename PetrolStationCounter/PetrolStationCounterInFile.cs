5a272270c90abc83293c966fe328baa8b1a3c9b6
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-11-04 16:37:17 +0100

    created methods AddLiters with additional parameter, statistics method added, and chooseFile method added.

--- PetrolStationCounter/PetrolStationCounterInFile.cs
+++ PetrolStationCounter/PetrolStationCounterInFile.cs
@@ -1,4 +1,7 @@
-﻿namespace PetrolStationCounter
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace PetrolStationCounter
 {
     internal class PetrolStationCounterInFile : BasePetrolStationCounter
     {
@@ -13,25 +16,96 @@
             : base(name, location)
         {
         }
-
-        public override void AddLiters(double liters)
+        
+        public override void AddLiters(double liters, string fileName)
         {
-            throw new NotImplementedException();
+            if (liters > 0)
+            {
+                using (var writer = File.AppendText(fileName))
+                {
+                    writer.WriteLine(liters);
+                    if (LitersAdded != null)
+                    {
+                        LitersAdded(this, new EventArgs());
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid grade value");
+            }
         }
 
-        public override void AddLiters(char liters)
+        public override void AddLiters(string liters, string fileName)
         {
-            throw new NotImplementedException();
+            if(double.TryParse(liters, out double result))
+            {
+                this.AddLiters(result, fileName);
+            }
+            else
+            {
+                throw new Exception("String is not a double!");
+            }
+        }      
+
+        public String ChooseFile(string input)
+        {
+            string fileName = "";
+
+            switch (input)
+            {
+                case "A":
+                    fileName = petrol95File;
+                    break;
+                case "B":
+                    fileName = petrol98File;
+                    break;
+                case "C":
+                    fileName = dieselFile;
+                    break;
+                case "D":
+                    fileName = dieselUltimateFile;
+                    break;
+                case "Q":
+                case "q":
+                        break;
+                        
+                default:
+                    throw new Exception("Wrong letter!");
+            }
+
+            return fileName;
         }
 
-        public override void AddLiters(string liters)
+        private List<double> GetLitersFromFile(string filename)
         {
-            throw new NotImplementedException();
+            var liters = new List<double>();
+            if(File.Exists(filename))
+            {
+                using(var reader = File.OpenText(filename))
+                {
+                    var line = reader.ReadLine();
+                    while(line != null)
+                    {
+                        double liter = double.Parse(line);
+                        liters.Add(liter);
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            return liters;
         }
 
-        public override Statistics GetStatistics()
+        public override Statistics GetStatistics(string fileName)
         {
-            throw new NotImplementedException();
+            var litersFromFile = this.GetLitersFromFile(fileName);
+            var statistics = new Statistics();
+            
+            foreach(var liter in litersFromFile)
+            {
+                statistics.AddLiters(liter);
+            }
+            return statistics;
         }
     }
 }
