d496a01e54af06c0a02e9030f86a32195fcf74cb
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-11-04 16:38:37 +0100

    Basic class implementation

---
+++ PetrolStationCounter/PetrolStationCounterInMemory.cs
@@ -0,0 +1,28 @@
+﻿namespace PetrolStationCounter
+{
+    internal class PetrolStationCounterInMemory : BasePetrolStationCounter
+    {
+        public override event LitersAddedDelegate LitersAdded;
+
+        public PetrolStationCounterInMemory(string name, string location)
+            : base(name, location)
+        {
+    
+        }
+
+        public override void AddLiters(double liters, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddLiters(string liters, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Statistics GetStatistics(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
