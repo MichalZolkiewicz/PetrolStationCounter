0e930a98876747c82b2602e752a9d79945f73e11
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-11-07 19:35:51 +0100

    Class to save data in memory created, two methods added.

--- PetrolStationCounter/PetrolStationCounterInMemory.cs
+++ PetrolStationCounter/PetrolStationCounterInMemory.cs
@@ -2,6 +2,11 @@
 {
     internal class PetrolStationCounterInMemory : BasePetrolStationCounter
     {
+        private List<double> petrol95 = new List<double>();
+        private List<double> petrol98 = new List<double>();
+        private List<double> dieselFuel = new List<double>();
+        private List<double> dieselUltimateFuel = new List<double>();
+        
         public override event LitersAddedDelegate LitersAdded;
 
         public PetrolStationCounterInMemory(string name, string location)
@@ -10,17 +15,46 @@
     
         }
 
-        public override void AddLiters(double liters, string fileName)
+        public override void AddLiters(double liters, List<double> fuelType)
         {
             throw new NotImplementedException();
         }
 
-        public override void AddLiters(string liters, string fileName)
+        public override void AddLiters(string liters, string type)
         {
             throw new NotImplementedException();
         }
 
-        public override Statistics GetStatistics(string fileName)
+        public List<double> ChooseList(string input)
+        {
+            List<double> fuelList;
+
+            switch (input)
+            {
+                case "A":
+                case "a":
+                    fuelList = this.petrol95;
+                    break;
+                case "B":
+                case "b":
+                    fuelList = this.petrol98;
+                    break;
+                case "C":
+                case "c":
+                    fuelList = this.dieselFuel;
+                    break;
+                case "D":
+                case "d":
+                    fuelList = this.dieselUltimateFuel;
+                    break;
+                default:
+                    throw new Exception("Wrong letter!");
+            }
+
+            return fuelList;
+        }
+
+        public override Statistics GetStatistics(string type)
         {
             throw new NotImplementedException();
         }
